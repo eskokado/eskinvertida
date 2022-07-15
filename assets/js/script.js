6fcc83276e15bebdbbb5a40b5d79c6fa46ce086e
eskokado <eskokado@gmail.com>
2022-07-15 05:13:46 -0300

    fix: criar card principal dinamico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,62 @@
-let addItem = document.querySelector(".addItem")
+let principalVitrine = document.querySelector('.principal__vitrine')
 let aside = document.querySelector('.principal__lista--right')
+for (i = 0; i < temporadas.length; i++) {
+    criarPrincipalItem(temporadas[i])
+}
+
+let addItem = document.querySelector(".addItem")
 
 let removeItem = document.querySelector('.removeItem')
 
 addItem.addEventListener('click', addItemReproducao)
 removeItem.addEventListener('click', removeItemReproducao)
 
+function criarPrincipalItem(temporada) {
+    let li_principal__vitrine = document.createElement('li')
+    let principal__item = document.createElement('article')
+    principal__item.classList.add('principal__item')
+
+    let principal__item_top = document.createElement('div')
+    principal__item_top.classList.add('principal__item--top')
+
+    let h4 = document.createElement('h4')
+    let span = document.createElement('span')
+
+    h4.innerHTML = temporada.nome
+    span.innerHTML = temporada.temporada
+    principal__item_top.appendChild(h4)
+    principal__item_top.appendChild(span)
+
+    let principal__item_bottom = document.createElement('div')
+    principal__item_bottom.classList.add('principal__item--bottom')
+
+    let p = document.createElement('p')
+    let a = document.createElement('a')
+
+    p.innerHTML = temporada.sinopse
+    a.classList.add('addItem')
+    a.id = temporada.id
+    a.href = '#'
+    a.innerHTML = 'Adiciona à fila'
+
+    principal__item_bottom.appendChild(p)
+    principal__item_bottom.appendChild(a)
+
+    principal__item.appendChild(principal__item_top)
+    principal__item.appendChild(principal__item_bottom)
+
+    li_principal__vitrine.appendChild(principal__item)
+
+    principalVitrine.appendChild(li_principal__vitrine)
+}
+
 function addItemReproducao(event) {
     criarItemReproducao();
 }
 
 function criarItemReproducao() {
-    let div_lateral__item = document.createElement('div')
-    div_lateral__item.classList.add('lateral__item')
+    let li_lateral__item = document.createElement('li')
+    li_lateral__item.classList.add('lateral__item')
 
     let img_lateral__item__left = document.createElement('img')
     img_lateral__item__left.classList.add('lateral__item--left')
@@ -25,32 +69,36 @@ function criarItemReproducao() {
     let p = document.createElement('p')
     p.innerHTML = 'Temporada 1'
     let a = document.createElement('a')
+    a.href = "#"    
     let a_img = document.createElement('img')
     a_img.src = './assets/img/play-icon.png'
     let a_span = document.createElement('span')    
     a_span.innerHTML = 'Assistir agora'
+    a.appendChild(a_img)
     div_lateral_item__center.appendChild(h4)
     div_lateral_item__center.appendChild(p)
-    div_lateral_item__center.appendChild(a_img)
+    div_lateral_item__center.appendChild(a)
 
-    let a_removeItem = document.createElement('a')
-    a_removeItem.classList.add('removeItem')
+    let button_removeItem = document.createElement('button')
+    button_removeItem.classList.add('removeItem')
+    button_removeItem.type = "button"
 
-    let a_img_removeItem = document.createElement('img')
-    a_img_removeItem.classList.add('lateral__item--right')
-    a_img_removeItem.src = './assets/img/trash-icon.png'
-    a_removeItem.appendChild(a_img_removeItem)
+    let button_img_removeItem = document.createElement('img')
+    button_img_removeItem.classList.add('lateral__item--right')
+    button_img_removeItem.src = './assets/img/trash-icon.png'
+    button_removeItem.appendChild(button_img_removeItem)
 
-    div_lateral__item.appendChild(img_lateral__item__left)
-    div_lateral__item.appendChild(div_lateral_item__center)
-    div_lateral__item.appendChild(a_removeItem)
+    li_lateral__item.appendChild(img_lateral__item__left)
+    li_lateral__item.appendChild(div_lateral_item__center)
+    li_lateral__item.appendChild(button_removeItem)
 
-    aside.appendChild(div_lateral__item)
+    aside.appendChild(li_lateral__item)
 }
 
 
 function removeItemReproducao(event) {
     event.preventDefault()
     const elementoHTML = event.target
-    elementoHTML.closest("div").remove()
+    elementoHTML.closest("li").remove()
+    console.log("Clicou trash")
 }
