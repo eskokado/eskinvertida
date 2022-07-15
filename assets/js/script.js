d018b8b73ffa14a78c5c85b9b9a5c72ca28d1648
eskokado <eskokado@gmail.com>
2022-07-15 08:14:21 -0300

    feat: finalizando scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 let principalVitrine = document.querySelector('.principal__vitrine')
 let aside = document.querySelector('.principal__lista--right')
-for (i = 0; i < temporadas.length; i++) {
-    criarPrincipalItem(temporadas[i])
-}
+let reproducaoLista = document.querySelector('.reproducao__lista')
+
+let temporadasSelecionadas = []
+
+listarTemporadas()
 
 let addItem = document.querySelector(".addItem")
+principalVitrine.addEventListener('click', addItemReproducao)
 
-let removeItem = document.querySelector('.removeItem')
-
-addItem.addEventListener('click', addItemReproducao)
-removeItem.addEventListener('click', removeItemReproducao)
+function listarTemporadas() {
+    for (let i = 0; i < temporadas.length; i++) {
+        criarPrincipalItem(temporadas[i])
+    }    
+}
 
 function criarPrincipalItem(temporada) {
     let li_principal__vitrine = document.createElement('li')
@@ -31,16 +35,15 @@ function criarPrincipalItem(temporada) {
     principal__item_bottom.classList.add('principal__item--bottom')
 
     let p = document.createElement('p')
-    let a = document.createElement('a')
+    let button = document.createElement('button')
 
     p.innerHTML = temporada.sinopse
-    a.classList.add('addItem')
-    a.id = temporada.id
-    a.href = '#'
-    a.innerHTML = 'Adiciona à fila'
+    button.classList.add('addItem')
+    button.id = temporada.id
+    button.innerHTML = 'Adiciona à fila'
 
     principal__item_bottom.appendChild(p)
-    principal__item_bottom.appendChild(a)
+    principal__item_bottom.appendChild(button)
 
     principal__item.appendChild(principal__item_top)
     principal__item.appendChild(principal__item_bottom)
@@ -51,23 +54,29 @@ function criarPrincipalItem(temporada) {
 }
 
 function addItemReproducao(event) {
-    criarItemReproducao();
+    const elementoHTML = event.target
+    if (elementoHTML.tagName == "BUTTON") {
+        const idTemporada = elementoHTML.id
+        const temporadaSelecionada = temporadas.find((temporada) => temporada.id == idTemporada)
+        temporadasSelecionadas.push(temporadaSelecionada)
+    }
+    listarTemporadasSelecionadas()
 }
 
-function criarItemReproducao() {
+function criarItemReproducao(temporada) {
     let li_lateral__item = document.createElement('li')
     li_lateral__item.classList.add('lateral__item')
 
     let img_lateral__item__left = document.createElement('img')
     img_lateral__item__left.classList.add('lateral__item--left')
-    img_lateral__item__left.src = './assets/img/imagemIcone.png'
+    img_lateral__item__left.src = temporada.imagemIcone
 
     let div_lateral_item__center = document.createElement('div')
     div_lateral_item__center.classList.add('lateral__item--center')
     let h4 = document.createElement('h4')
-    h4.innerHTML = 'Maratona Invertida'
+    h4.innerHTML = temporada.nome
     let p = document.createElement('p')
-    p.innerHTML = 'Temporada 1'
+    p.innerHTML = temporada.temporada
     let a = document.createElement('a')
     a.href = "#"    
     let a_img = document.createElement('img')
@@ -81,24 +90,44 @@ function criarItemReproducao() {
 
     let button_removeItem = document.createElement('button')
     button_removeItem.classList.add('removeItem')
-    button_removeItem.type = "button"
+    // button_removeItem.type = "button"
+    button_removeItem.id = temporada.id
 
-    let button_img_removeItem = document.createElement('img')
-    button_img_removeItem.classList.add('lateral__item--right')
-    button_img_removeItem.src = './assets/img/trash-icon.png'
-    button_removeItem.appendChild(button_img_removeItem)
+    // let button_img_removeItem = document.createElement('img')
+    // button_img_removeItem.classList.add('lateral__item--right')
+    // button_img_removeItem.src = './assets/img/trash-icon.png'
+    // button_removeItem.appendChild(button_img_removeItem)
 
     li_lateral__item.appendChild(img_lateral__item__left)
     li_lateral__item.appendChild(div_lateral_item__center)
     li_lateral__item.appendChild(button_removeItem)
 
-    aside.appendChild(li_lateral__item)
+    reproducaoLista.appendChild(li_lateral__item)
 }
 
+function listarTemporadasSelecionadas() {
+    reproducaoLista.innerHTML = ""
+    for (let i = 0; i < temporadasSelecionadas.length; i++) {
+        criarItemReproducao(temporadasSelecionadas[i])
+    }
+}
 
 function removeItemReproducao(event) {
-    event.preventDefault()
     const elementoHTML = event.target
-    elementoHTML.closest("li").remove()
-    console.log("Clicou trash")
+    if (elementoHTML.tagName == "BUTTON") {
+        const idTemporada = elementoHTML.id
+        elementoHTML.closest("li").remove()
+
+        const temporadaSelecionada = temporadasSelecionadas.find((temporada) => temporada.id == idTemporada)
+
+        elementoHTML.closest("li").remove()
+        
+        const index = temporadasSelecionadas.indexOf(temporadaSelecionada)
+
+        temporadasSelecionadas.splice(index, 1)
+
+        console.log(temporadasSelecionadas)
+    }    
 }
+
+reproducaoLista.addEventListener('click', removeItemReproducao)
